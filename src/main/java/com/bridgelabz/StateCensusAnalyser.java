ac7e7529dd8234435d6d7d3d2ca261abed1c196e
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-07-23 22:27:56 +0530

    Inital Setup

---
+++ src/main/java/com/bridgelabz/StateCensusAnalyser.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+public class StateCensusAnalyser {
+
+	
+}
