6c461516e5950fae2a00045a691d00260c65bb6e
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-07-23 23:46:05 +0530

    Added: to check number of records in the file

--- src/main/java/com/bridgelabz/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/StateCensusAnalyser.java
@@ -5,10 +5,41 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
-
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+
 public class StateCensusAnalyser {
 
+	public static int LoadIndiaCensusData(String csvPath)   {
+		
+		Reader reader = null;
+			try {
+				reader = Files.newBufferedReader(Paths.get(csvPath));
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+			}
+		
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		CsvToBean <IndianCensusCSV> csvToBean = new CsvToBeanBuilder(reader)
+                                      .withType(IndianCensusCSV.class)
+                                      .withIgnoreLeadingWhiteSpace(true)
+                                      .build();
+		Iterator<IndianCensusCSV> censusCSVIterator = csvToBean.iterator();;
+		int entries = 0;
+		while(censusCSVIterator.hasNext()) {
+			entries++;
+			IndianCensusCSV censusData = censusCSVIterator.next();
+		}
+		System.out.println("Number of Entries in File"+entries);
+		return entries;	
+		
+	}
 	
+	public static void main(String[] args)  {
+		
+		String csvPath = "C:\\Users\\Jagruti Khichi\\Eclipse-workspase\\IndianStatesCensusAnalyser\\Data\\censusFile.csv";
+		LoadIndiaCensusData(csvPath);
+		
+	}
 }
